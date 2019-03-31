f0da24cdf49dac2cd07bbd71c1fe725e808a1a1f
sergio41 <sergiorr41@gmail.com>
2019-03-31 20:13:07 +0200

    Include Port in response

--- microservices/currency-exchange-service/src/main/java/com/cursos/microservices/currencyexchangeservice/CurrencyExchangeController.java
+++ microservices/currency-exchange-service/src/main/java/com/cursos/microservices/currencyexchangeservice/CurrencyExchangeController.java
@@ -2,15 +2,22 @@ package com.cursos.microservices.currencyexchangeservice;
 
 import java.math.BigDecimal;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CurrencyExchangeController {
+	
+	@Autowired
+	private Environment environment;
 
-	@GetMapping("/currency-extranve/from/{from}/to/{to}")
+	@GetMapping("/currency-exchange/from/{from}/to/{to}")
 	public ExchangeValue retrieveExchangeValue(@PathVariable String from, @PathVariable String to) {
-		return new ExchangeValue(1000l, from, to, BigDecimal.valueOf(67));
+		ExchangeValue exchangeValue = new ExchangeValue(1000l, from, to, BigDecimal.valueOf(67));
+		exchangeValue.setPort(Integer.parseInt(environment.getProperty("local.server.port")));
+		return exchangeValue;
 	}
 }
