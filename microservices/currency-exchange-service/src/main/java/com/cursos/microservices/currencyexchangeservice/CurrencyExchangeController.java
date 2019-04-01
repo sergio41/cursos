8fe8ba44ed62e80b1bad179b2e15d782e7f70056
sergio41 <sergiorr41@gmail.com>
2019-04-01 22:51:12 +0200

    JPA Repository with find by 2 columns

--- microservices/currency-exchange-service/src/main/java/com/cursos/microservices/currencyexchangeservice/CurrencyExchangeController.java
+++ microservices/currency-exchange-service/src/main/java/com/cursos/microservices/currencyexchangeservice/CurrencyExchangeController.java
@@ -1,7 +1,5 @@
 package com.cursos.microservices.currencyexchangeservice;
 
-import java.math.BigDecimal;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,10 +11,13 @@ public class CurrencyExchangeController {
 	
 	@Autowired
 	private Environment environment;
+	
+	@Autowired
+	private ExchangeValueRepository repository;
 
 	@GetMapping("/currency-exchange/from/{from}/to/{to}")
 	public ExchangeValue retrieveExchangeValue(@PathVariable String from, @PathVariable String to) {
-		ExchangeValue exchangeValue = new ExchangeValue(1000l, from, to, BigDecimal.valueOf(67));
+		ExchangeValue exchangeValue = repository.findByFromAndTo(from, to);
 		exchangeValue.setPort(Integer.parseInt(environment.getProperty("local.server.port")));
 		return exchangeValue;
 	}
---
+++ microservices/currency-exchange-service/src/main/java/com/cursos/microservices/currencyexchangeservice/ExchangeValueRepository.java
@@ -0,0 +1,10 @@
+package com.cursos.microservices.currencyexchangeservice;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ExchangeValueRepository extends JpaRepository<ExchangeValue, Long>{
+
+	ExchangeValue findByFromAndTo(String from, String to);
+}
