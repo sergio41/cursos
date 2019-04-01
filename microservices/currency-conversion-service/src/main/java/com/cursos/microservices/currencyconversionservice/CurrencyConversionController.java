5c0eb54004b8a3659173fa09f26b50c67ed6d755
sergio41 <sergiorr41@gmail.com>
2019-04-01 23:48:39 +0200

    remove unused imports

--- microservices/currency-conversion-service/src/main/java/com/cursos/microservices/currencyconversionservice/CurrencyConversionController.java
+++ microservices/currency-conversion-service/src/main/java/com/cursos/microservices/currencyconversionservice/CurrencyConversionController.java
@@ -4,8 +4,6 @@ import java.math.BigDecimal;
 import java.util.HashMap;
 import java.util.Map;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
