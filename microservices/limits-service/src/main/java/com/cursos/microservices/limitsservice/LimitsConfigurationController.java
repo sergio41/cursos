e440065a809f1edf2fe056834c9b4bda44759d73
sergio41 <sergiorr41@gmail.com>
2019-04-14 02:15:16 +0200

    Fault Tolerance

--- microservices/limits-service/src/main/java/com/cursos/microservices/limitsservice/LimitsConfigurationController.java
+++ microservices/limits-service/src/main/java/com/cursos/microservices/limitsservice/LimitsConfigurationController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cursos.microservices.limitsservice.bean.LimitsConfiguration;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 
 @RestController
 public class LimitsConfigurationController {
@@ -15,5 +16,15 @@ public class LimitsConfigurationController {
 	@GetMapping("/limits")
 	public LimitsConfiguration retrieveLimitsFromConfiguration() {
 		return new LimitsConfiguration(configuration.getMaximum(), configuration.getMinimum());
+	}
+	
+	@GetMapping("/limits-fault")
+	@HystrixCommand(fallbackMethod="fallbackRetrieveConfiguration")
+	public LimitsConfiguration retrieveFaultToleranceExample() {
+		throw new RuntimeException("Error");
+	}
+	
+	public LimitsConfiguration fallbackRetrieveConfiguration() {
+		return new LimitsConfiguration(999, 9);
 	}
 }
